f7f9ecb4083c3dc514e4cd20c8a41fbe2d145659
Aldycky1 <bagusaldycky@gmail.com>
2021-10-30 10:03:47 +0700

    add new export buku_kita.sql file

--- buku_kita.sql
+++ buku_kita.sql
@@ -21,7 +21,10 @@ CREATE TABLE `buku` (
 
 INSERT INTO `buku` (`id`, `judul`, `penerbit`, `pengarang`, `tahun`, `kategori_id`, `harga`) VALUES
 (2,	'Buku Agama',	'Gramedia',	'Roy Suryo',	'2001',	NULL,	9999),
-(3,	'Buku Belajar Memahami Wanita',	'Gramedia',	'Herzi',	'2020',	53,	99000);
+(4,	'Mastering React JS',	'Gramedia',	'Aldycky Bagus ',	'2020',	61,	399000),
+(5,	'Jago Bahasa Java Dalam Sebulan',	'Erlangga',	'Aldycky Bagus ',	'2019',	62,	699000),
+(6,	'Bangun Website Sederhana dengan HTML & CSS',	'Yudhistira ',	'Aldycky Bagus ',	'2021',	64,	299000),
+(7,	'Mahir Menggunakan Bahasa PHP ',	'Gramedia',	'Aldycky Bagus ',	'2020',	63,	499000);
 
 SET NAMES utf8mb4;
 
@@ -36,7 +39,8 @@ INSERT INTO `divisi` (`id`, `nama_divisi`) VALUES
 (2,	'HRD'),
 (3,	'Marketing'),
 (4,	'Personalia'),
-(5,	'Pembelanjaan');
+(5,	'Pembelanjaan'),
+(6,	'Divisi Baru');
 
 DROP TABLE IF EXISTS `kategori`;
 CREATE TABLE `kategori` (
@@ -46,67 +50,29 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `kategori` (`id`, `nama_kategori`) VALUES
-(6,	'Rhome irama'),
-(7,	'lucuu2'),
-(8,	'asiap1'),
-(10,	'12'),
-(11,	'12'),
-(12,	'12'),
-(13,	'12'),
-(14,	'12'),
-(15,	'qw'),
-(16,	'qw'),
-(17,	'qw'),
-(18,	'qw'),
-(19,	'qw'),
-(20,	'123'),
-(21,	'44'),
-(22,	'qd'),
-(23,	'ef'),
-(24,	'24'),
-(25,	'qwd'),
-(26,	'qwd'),
-(27,	'qwd'),
-(28,	'qwd'),
-(29,	'qwd'),
-(31,	'123'),
-(32,	'qwd'),
-(33,	'asd'),
-(34,	'asd'),
-(35,	'asd'),
-(36,	'123'),
-(37,	'qwd'),
-(38,	'asd'),
-(39,	'asd'),
-(40,	'asd'),
-(41,	'asc'),
-(42,	'123'),
-(43,	'asd'),
-(44,	'asd'),
-(48,	'bb'),
-(49,	'5000'),
-(50,	'Anime'),
-(52,	'123'),
-(53,	'Love Story'),
-(54,	'2342'),
-(55,	'2342'),
-(56,	'2342'),
-(57,	'123'),
-(58,	'tambah');
+(61,	'JavaScript'),
+(62,	'Java'),
+(63,	'PHP'),
+(64,	'HTML & CSS');
 
 DROP TABLE IF EXISTS `pegawai`;
 CREATE TABLE `pegawai` (
+  `nip` varchar(20) NOT NULL,
   `divisi_id` int(11) NOT NULL,
-  `nip` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(50) NOT NULL,
   `alamat` varchar(75) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `no_hp` varchar(20) NOT NULL,
-  PRIMARY KEY (`nip`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `pegawai` (`divisi_id`, `nip`, `nama`, `alamat`, `tanggal_lahir`, `no_hp`) VALUES
-(5,	2,	'Aldycky Bagus Oke',	'Bekasi, Jawa Barat',	'2021-10-10',	'081244359880');
+INSERT INTO `pegawai` (`nip`, `divisi_id`, `id`, `nama`, `alamat`, `tanggal_lahir`, `no_hp`) VALUES
+('3248738298094',	3,	8,	'Aldycky Bagus',	'Bekasi, Jawa Barat',	'2000-10-20',	'082100000000'),
+('83782192837',	2,	9,	'Elon Musk',	'Bandung, Jawa Barat',	'1971-06-28',	'+6281300000000'),
+('2832837823781',	5,	10,	'Mark Zuckerberg',	'Solo, Jawa Tengah',	'1984-05-14',	'+627800000000'),
+('3248738294324',	3,	11,	'Bill Gates',	'Surabaya, Jawa Timur',	'1955-10-28',	'+6283800000000'),
+('3248723032238',	6,	12,	'Steve Jobs',	'Jaksel, DKI Jakarta',	'1955-02-24',	'+6289600000000');
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -120,4 +86,4 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `nama`, `username`, `password`) VALUES
 (1,	'Herzi',	'admin',	'21232f297a57a5a743894a0e4a801fc3');
 
--- 2021-10-12 08:30:17
+-- 2021-10-30 03:01:58
